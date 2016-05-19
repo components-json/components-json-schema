83e65a4e4fbc1a5575de1182d5d7c3c211fc5f0b
Evan Lovely <evan@evanlovely.com>
2016-05-19 11:03:55 -0700

    adding multiple test

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 'use strict';
 var componentsJsonSchema = require('./index.js');
 var fs = require('fs');
+var glob = require('glob');
 
-var x = JSON.parse(fs.readFileSync('./examples/button/components.json', {
-  encoding: 'utf8'
-}));
-
-var passed = componentsJsonSchema.validate(x);
-if (passed) {
-  console.log("passed");
-} else {
-  console.log("did not pass");
-}
+glob('./examples/**/components.json', function(err, files) {
+  files.forEach(function(file) {
+    fs.readFile(file, 'utf8', function(err, fileContents) {
+      var parsedContents = JSON.parse(fileContents);
+      var passed = componentsJsonSchema.validate(parsedContents);
+      if (passed) {
+        console.log("passed: ", file);
+      } else {
+        console.log("did not pass: ", file);
+        process.exit(1);
+      }
+    });
+  });
+});
